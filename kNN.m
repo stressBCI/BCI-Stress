af8175e8e4b3598dfd5f6cd542dfc47f7f953577
SVstudent <79673560+SVstudent@users.noreply.github.com>
2024-03-15 16:12:40 -0700

    Update kNN.m
    Here

--- kNN.m
+++ kNN.m
@@ -1,4 +1,4 @@
-
+% Sample feature matrix
 % If using for actual files: do: feature_matrix = load(data_file.txt)
 feature_matrix = [
     0.0900, 0.0162, 0.0068, 0.0081, 1.3478, 1.8924, 1.0000; % Channel 1 A01_stress_mat
@@ -36,10 +36,26 @@ feature_matrix = [
     0.0914, 0.0184, 0.0074, 0.0087, 1.4100, 1.9266, 0; % Channel 3 A02_normal_mat
     0.0899, 0.0175, 0.0075, 0.0084, 1.3260, 1.8895, 0; % Channel 4 A02_normal_mat
 
-    0.0896,	0.0175,	0.0073,	0.0083,	1.3042,	1.8802,	0;
-    0.0934,	0.0180,	0.0077,	0.0087,	1.3515,	1.9132,	0;
-    0.0877,	0.0171,	0.0073,	0.0082,	1.2849,	1.8630,	0;
-    0.0889, 0.0173	0.0073,	0.0083,	1.2905,	1.8694,	0;
+    0.0896,	0.0175,	0.0073,	0.0083,	1.3042,	1.8802,	0; %Normal
+    0.0934,	0.0180,	0.0077,	0.0087,	1.3515,	1.9132,	0; %Normal
+    0.0877,	0.0171,	0.0073,	0.0082,	1.2849,	1.8630,	0; %Normal
+    0.0889, 0.0173	0.0073,	0.0083,	1.2905,	1.8694,	0; %Normal
+
+    0.0885, 0.0172, 0.0073, 0.0083, 1.2959, 1.8712, 0; %Normal
+    0.0850, 0.0171, 0.0073, 0.0085, 1.3324, 1.8792, 0; %Normal
+    0.0875, 0.0169, 0.0071, 0.0083, 1.3041, 1.8686, 0; %Normal
+    0.0847, 0.0170, 0.0070, 0.0081, 1.2973, 1.8633, 0; %Normal
+    
+    0.0841, 0.0165,	0.0071,	0.0079,	1.2477,	1.8361,	1.0000;
+    0.0862, 0.0169,	0.0072, 0.0081,	1.2652,	1.8498,	1.0000;
+    0.0851,	0.0167,	0.0071, 0.0079,	1.2554,	1.8412,	1.0000;
+    0.0815, 0.0159,	0.0069, 0.0077,	1.2540,	1.8296,	1.0000;
+    
+    0.0848,	0.0163,	0.0070,	0.0080,	1.2844,	1.8614, 0;
+    0.0852,	0.0160,	0.0069,	0.0078,	1.2544,	1.8457, 0;
+    0.0877,	0.0171,	0.0072,	0.0081,	1.2839, 1.8642, 0;
+    0.0910,	0.0176,	0.0075,	0.0086,	1.3304,	1.8975, 0;
+    
 ];
 
 X = feature_matrix(:, 1:end-1); % Features
@@ -47,7 +63,7 @@ Y = feature_matrix(:, end); % Labels
 
 % Split data into training and testing sets (20% training, 80% testing)
 rng(1); % For reproducibility
-cv = cvpartition(size(X, 1), 'HoldOut', 0.1); % Change HoldOut to 0.8 for 80% testing
+cv = cvpartition(size(X, 1), 'HoldOut', 0.5); % Change HoldOut to 0.8 for 80% testing
 X_train = X(training(cv), :);
 Y_train = Y(training(cv), :);
 X_test = X(test(cv), :);
@@ -56,44 +72,13 @@ Y_test = Y(test(cv), :);
 
 
 % Create and train the K-NN model
-K = 16; % Number of neighbors
+K = 2; % Number of neighbors
 mdl = fitcknn(X_train, Y_train, 'NumNeighbors', K);
 
-% TRAINING MODEL ENDS_____________________________________________________________________________
+% Predict labels for the test set
+Y_pred = predict(mdl, X_test);
 
-
-%%
-%%
-%%
-%%
-%%
-%%
-%%
-
-
-%INPUT WHATEVER YOU WANT INTO THE HELP VARIABLE - YOU CAN EVEN IMPORT FILE
-
-
-
-%Predict labels for the test set
-%Y_pred = predict(mdl, X_test);
-%display(Y_pred)
-
-help = [0.0896,	0.0175,	0.0073,	0.0083,	1.3042,	1.8802;
-        0.0934,	0.0180,	0.0077,	0.0087,	1.3515,	1.9132;
-        0.0877,	0.0171,	0.0073,	0.0082,	1.2849,	1.8630;
-        0.0889, 0.0173	0.0073,	0.0083,	1.2905,	1.8694; ]; %Put MATRIX IN HERE CHANGE VARIABLE HOWEVER YOU WOULD LIKE TO
-
-pred = mode(predict(mdl, help));
-
-
-if pred == 0
-    final_pred_dis = 'Stress';
-else
-    final_pred_dis = "No Stress";
-end
-
-display(final_pred_dis)
+display(Y_pred)
 % Separation between the training part
 % Result as Word stress or no stress
 
